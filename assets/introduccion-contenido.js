9e8fd12dcbb122c5c0b735b079ec4df29c7078d3
ikaronauta <jaortiz0496@misena.edu.co>
2022-08-18 15:38:20 -0500

    update inicio

--- assets/introduccion-contenido.js
+++ assets/introduccion-contenido.js
@@ -1,30 +1,33 @@
 export const contenidoIntroduccion = [
   {
     id: 1,
-    titulo: "Bienvenidos",
-    contenido:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non maximus diam. Fusce sagittis augue in mi elementum accumsan. Quisque vulputate velit non leo facilisis accumsan. Aenean gravida tortor id scelerisque elementum. Pellentesque feugiat rhoncus pharetra. Quisque egestas accumsan lacinia. Phasellus eu mollis dolor. Morbi hendrerit sollicitudin ante at tempus. Integer mattis nibh nec magna venenatis aliquam. Donec metus orci, commodo ac felis eu, suscipit tempor leo. Vestibulum magna diam, porttitor ac efficitur nec, volutpat posuere ipsum. Ut venenatis malesuada ipsum, at tincidunt quam commodo eget. Morbi semper ante orci, ac ultrices tellus sollicitudin et.",
     imagen: "img1.jpg",
+    mensaje:
+      "Detesté cada minuto de entrenamiento, pero me dije: No renuncies, sufre ahora y vive el resto de tu vida como un campeón",
+    autor: "Michael Jordan",
+    imagenAutor: "michael-jordan.jpg",
   },
   {
     id: 2,
-    titulo: "вітаем",
-    contenido:
-      "Nulla ullamcorper purus vel eros dapibus, vitae posuere neque tincidunt. Sed id rutrum dolor. Morbi vel dolor lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse consequat ex ac placerat tincidunt. Sed sit amet cursus felis. Aliquam erat volutpat. Ut in dictum neque. Sed volutpat molestie vestibulum. Mauris maximus nisi non lectus pellentesque venenatis sit amet sed enim. Praesent ultrices viverra lectus, sit amet lacinia nibh feugiat quis. Curabitur laoreet turpis nec auctor egestas. Praesent vel risus euismod, congue est id, rhoncus purus. Donec vel urna eu quam sollicitudin fringilla ut a sem.",
     imagen: "img2.jpg",
+    mensaje:
+      "Los campeones no se hacen en los gimnasios. Los campeones están hechos de algo que tienen muy adentro de ellos: el deseo, un sueño y una visión",
+    autor: "Muhammad Ali",
+    imagenAutor: "muhammad-ali.jpg",
   },
   {
     id: 3,
-    titulo: "Bonjour",
-    contenido:
-      "Phasellus tristique porta ex id congue. Sed suscipit elit mauris, non porttitor nibh commodo non. Donec id nisl nunc. Nunc interdum tellus ipsum, vehicula consequat risus consectetur a. Nunc luctus ante sed tellus condimentum, porttitor vehicula ipsum sollicitudin. Aenean ac augue in odio laoreet dictum eget rutrum ligula. Morbi vel purus vulputate, imperdiet arcu quis, tempus lorem. Fusce bibendum condimentum augue, vel porta ipsum convallis eu.",
     imagen: "img3.jpg",
+    mensaje:
+      "No importa lo bueno que seas, lo emocionante es que puedes ser todavía mejor.",
+    autor: "Tiger Woods",
+    imagenAutor: "tiger-woods.jpg",
   },
   {
     id: 4,
-    titulo: "mirëseardhje",
-    contenido:
-      "Etiam in consequat risus, aliquet blandit elit. Aliquam auctor velit sit amet leo vehicula, quis dignissim enim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id neque sit amet lacus mollis gravida non sed velit. Praesent efficitur, lectus eget ullamcorper consectetur, quam libero pulvinar metus, sed ullamcorper lacus felis at lectus. Proin vitae hendrerit orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis pharetra interdum ullamcorper. Nam bibendum sit amet dui sed condimentum. Aenean fringilla feugiat lacus, eu bibendum justo sollicitudin vel. Ut eu elit congue, interdum sapien id, blandit ante. Aenean tincidunt egestas malesuada. Nunc viverra cursus condimentum. Phasellus pellentesque posuere commodo. Donec sit amet posuere enim. Duis mollis dictum sem, sed fringilla enim.",
     imagen: "img4.jpg",
+    mensaje: "No te puedes poner ningún límite, no hay nada imposible.",
+    autor: "Usain Bolt",
+    imagenAutor: "usain-bolt.jpg",
   },
 ];
--- pages/inicio.js
+++ pages/inicio.js
@@ -2,7 +2,11 @@ import { contenidoIntroduccion } from "../assets/introduccion-contenido.js";
 import { removeChild } from "../components/remove-child.js";
 
 const d = document;
-const pathImg = "images/inicio/";
+const pathImg = "images/inicio/principal/";
+const pathImgAutor = "images/inicio/autores/";
+const titulo = "Bienvenidos";
+const textoInicio =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non maximus diam. Fusce sagittis augue in mi elementum accumsan. Quisque vulputate velit non leo facilisis accumsan. Aenean gravida tortor id scelerisque elementum.";
 
 let ir = Math.floor(Math.random() * contenidoIntroduccion.length);
 
@@ -11,19 +15,11 @@ export function inicio() {
   crearContenedores();
 }
 
-function titulo() {
-  let titulo = d.createTextNode("Introducción");
-  let h1 = d.createElement("h1");
-  h1.appendChild(titulo);
-
-  d.getElementById("root").appendChild(h1);
-}
-
 function crearContenedores() {
   let img = d.createElement("img");
   img.classList.add("introduccion-img");
   img.src = pathImg + contenidoIntroduccion[ir].imagen;
-  img.setAttribute("alt", contenidoIntroduccion[ir].titulo);
+  img.setAttribute("alt", "Bienvenidos");
 
   let contenedorTexto = d.createElement("div");
   contenedorTexto.classList.add("introduccion-contenedor-texto");
@@ -41,11 +37,46 @@ function crearContenedores() {
 
 function cargarTextos(contenedor) {
   let h2 = d.createElement("h2");
-  let textoTitulo = d.createTextNode(contenidoIntroduccion[ir].titulo);
+  let textoTitulo = d.createTextNode(titulo);
   h2.appendChild(textoTitulo);
 
-  let textoContenido = d.createTextNode(contenidoIntroduccion[ir].contenido);
+  let p1 = d.createElement("p");
+  let textoContenido = d.createTextNode(textoInicio);
+  p1.appendChild(textoContenido);
 
   contenedor.appendChild(h2);
-  contenedor.appendChild(textoContenido);
+  contenedor.appendChild(p1);
+  mensajes(contenedor);
+}
+
+function mensajes(contenedor) {
+  let mensaje = d.createTextNode('"' + contenidoIntroduccion[ir].mensaje + '"');
+  let p = d.createElement("p");
+  p.classList.add("inicio-mensaje");
+  p.appendChild(mensaje);
+
+  let div = d.createElement("div");
+  div.classList.add("inicio-contenedor-mensaje");
+  div.appendChild(p);
+
+  contenedor.appendChild(div);
+  autores(contenedor);
+}
+
+function autores(contenedor) {
+  let autor = d.createTextNode(contenidoIntroduccion[ir].autor);
+  let imgAutor = d.createElement("img");
+  imgAutor.src = pathImgAutor + contenidoIntroduccion[ir].imagenAutor;
+  imgAutor.classList.add("inicio-imagen-autor");
+
+  let b = d.createElement("b");
+  b.classList.add("inicio-autor");
+  b.appendChild(autor);
+
+  let div = d.createElement("div");
+  div.classList.add("inicio-contenedor-autor");
+  div.appendChild(b);
+  div.appendChild(imgAutor);
+
+  contenedor.appendChild(div);
 }
