c30dd8842494f0f6541dae1d992ab9860c929a81
ikaronauta <jaortiz0496@misena.edu.co>
2022-08-23 14:15:00 -0500

    update

--- components/remove-child.js
+++ components/remove-child.js
@@ -1,5 +1,9 @@
 const d = document;
 
 export function removeChild() {
-  if (d.getElementById("root").hasChildNodes()) d.getElementById("root").removeChild(d.getElementById("root").childNodes[0]);
+  if (d.getElementById("root").hasChildNodes())
+    d.getElementById("root").removeChild(
+      d.getElementById("root").childNodes[0]
+    );
+  if (d.getElementById("back")) d.getElementById("back").remove();
 }
